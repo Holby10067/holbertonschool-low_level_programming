6a2efad737ede0ed6b17036d3664c3e672860084
Holby10067 <10067@holbertonstudents.com>
2024-10-21 12:47:55 +0000

    Add script: 4-puts.c

---
+++ hello_world/4-puts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This program  prints the outpout (Programming is like building a multilingual puzzle)
+ * follow by a newline
+ *
+ * Return: Alway 0 (Success)
+ *
+ */
+
+int main() {
+
+	/* Using puts func to print string */
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return 0;
+}
