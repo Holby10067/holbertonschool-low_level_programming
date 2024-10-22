966c77da34256664064b350fa2236b10d084ad39
Holby10067 <10067@holbertonstudents.com>
2024-10-22 10:37:36 +0000

    print out positive or negative base on value

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,37 @@
+/* C Runtime liberary header files */
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This Program check and print and message out
+ * whether the value of n is possitive or negative.
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+		printf("%d is positive", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero ", n);
+	}
+	else
+	{
+		printf("%d is negative ", n);
+	}
+
+	return (0);
+}
