bfc88cc338e69857fcf9ba66177df159fc0f1a06
Holby10067 <10067@holbertonstudents.com>
2024-10-23 07:41:18 +0000

    Print letters in lowercase accept '\e\' and '\q\'

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,29 @@
+/* C Runtime liberary header files */
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints the alphabet in lowercase accept '\e\' and '\q\'
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+
+	for (char letters = 'a'; letters <= 'z'; letters++)
+	{
+		if (letters == 'e' || letters == 'q')
+		{
+			continue;/* ship to the next char */
+		}
+		putchar(letters);
+	}
+	putchar('\n');
+
+	return (0);
+}
