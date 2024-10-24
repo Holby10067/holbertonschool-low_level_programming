c0326b70c50d1da55bd00ea013539b1d1c2c3a41
Holby10067 <10067@holbertonstudents.com>
2024-10-24 21:29:19 +0000

    Using func from user defined header file ** main.h**

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+/* user defined header files */
+#include "main.h"
+
+/**
+ * print_alphabet_10x - print to stdout
+ *
+ * Description: print_alphabet_x10 - print alphabet in lowercase x10 to stdout
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char letters;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (letters = 'a'; letters <= 'z'; letters++)
+		{
+			_putchar(letters);
+		}
+		_putchar('\n');
+	}
+}
