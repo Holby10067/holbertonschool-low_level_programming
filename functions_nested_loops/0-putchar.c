681fc3a76003645e43008db066207e298c007a95
Holby10067 <10067@holbertonstudents.com>
2024-10-24 14:06:25 +0000

    Task 0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+
+/**
+ * Description: print_putchard - print the characters '_putchar' to stdout
+ *
+ * Return: Always 0 (Success)
+ */
+
+int print_putchar(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	return (0);
+
+}
